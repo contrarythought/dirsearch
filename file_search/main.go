330d79d2b4ad76176b2492da2e9e57e108cebda7
Anthony Thorpe <59274215+contrarythought@users.noreply.github.com>
2022-07-24 17:39:41 -0400

    Update main.go

--- file_search/main.go
+++ file_search/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"file_search/workers"
 	"fmt"
 	"log"
 	"os"
@@ -13,19 +12,15 @@ const (
 )
 
 func ReadDirRecur(startPath string, target string) bool {
-	fmt.Println("Opening dir: ", startPath)
 	dirEntries, err := os.ReadDir(startPath + "\\")
 	if err != nil {
-		fmt.Println(err.Error())
 		return false
 	}
 
 	var found bool = false
 
 	for _, entry := range dirEntries {
-		fmt.Println("\tlooking at: ", entry.Name())
 		if strings.Contains(entry.Name(), target) && entry.IsDir() == false {
-			println("found: ", entry.Name())
 			return true
 		}
 		if entry.IsDir() {
@@ -41,9 +36,6 @@ func main() {
 	if len(os.Args) != 2 {
 		log.Fatalf("usage <file to search>\n")
 	}
-
-	worker_pool := workers.NewWorkerPool(workers.MAX_WORKERS)
-
 	if found := ReadDirRecur(ROOT_PATH, os.Args[1]); found {
 		fmt.Println("Successfully found: ", os.Args[1])
 	} else {
